ba3a574f83d506e7b210d498bfa6815a96af1174
Martin Nazaryan <38017326+martin-nazaryan@users.noreply.github.com>
2023-09-18 21:48:14 +0400

    Added health check endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-import { ConfigModule } from '@nestjs/config';
 
 import { CountriesModule } from './resources/countries/countries.module';
 import { InfoModule } from './resources/info/info.module';
@@ -8,19 +7,20 @@ import { CountriesController } from './resources/countries/countries.controller'
 import { CountriesService } from './resources/countries/countries.service';
 import { InfoController } from './resources/info/info.controller';
 import { InfoService } from './resources/info/info.service';
+import { EnvironmentModule } from './environment/environment.module';
+import { HealthModule } from './resources/health/health.module';
 
 import * as process from 'process';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env', '.env.local'],
-    }),
     HttpModule.register({
       baseURL: process.env.API_URL,
     }),
+    EnvironmentModule,
     CountriesModule,
     InfoModule,
+    HealthModule,
   ],
   controllers: [CountriesController, InfoController],
   providers: [CountriesService, InfoService],
--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.enableShutdownHooks();
 
   const config = new DocumentBuilder()
     .setTitle('Covid-19 API example')
     .setDescription('lorem ipsum')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
